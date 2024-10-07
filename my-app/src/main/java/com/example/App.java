706b3982c64ddb7fefa23495d25018f16aad98e2
Venkatesh Thadikamalla <venkatesh.thadikamalla@odevo.com>
2024-10-07 15:31:44 +0200

    BREAKING CHANGE: Remove the welcome message in app.java

--- my-app/src/main/java/com/example/App.java
+++ my-app/src/main/java/com/example/App.java
@@ -2,13 +2,12 @@ package com.example;
 
 /**
  * Hello world!
- * Welcome to Odevo !!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Odevo! \n Hello World!" );
+        System.out.println( "Hello World!" );
     }
 }
