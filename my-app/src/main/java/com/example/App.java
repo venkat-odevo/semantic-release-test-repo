220484ba94f6f760a80b174dd996883f7e0ae347
Venkatesh Thadikamalla <venkatesh.thadikamalla@odevo.com>
2024-10-07 15:26:32 +0200

    perf: Improve app performance

--- my-app/src/main/java/com/example/App.java
+++ my-app/src/main/java/com/example/App.java
@@ -9,7 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Odevo!" );
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to Odevo! \n Hello World!" );
     }
 }
